db3b967ff3a87cba7b01d86e3cac0afc76ba7a28
Gabriel Fairbairn <gabrielfair@hotmail.com>
2023-09-11 00:21:29 -0700

    fix: global css formatting, chakra/next wrappers

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,10 +1,18 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { ColorModeScript } from "@chakra-ui/react";
+import theme from "@/styles/theme";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>COMP 4721 Project 1 by Gabriel and Tommy</title>
+        <meta name="description" content="assignment by Patrick Guichon" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <Main />
         <NextScript />
       </body>
