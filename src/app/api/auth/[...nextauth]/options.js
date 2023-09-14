0c13c182cb652e7c7cd91c2ee3a179a7fd448b3a
Gabriel Fairbairn <gabrielfair@hotmail.com>
2023-09-14 00:47:05 -0700

    test: sessions and mongo adapter

--- src/app/api/auth/[...nextauth]/options.js
+++ src/app/api/auth/[...nextauth]/options.js
@@ -1,12 +1,43 @@
 import NextAuth from "next-auth";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import clientPromise from "../../../lib/mongo";
 import GitHubProvider from "next-auth/providers/github";
 import CredentialsProvider from "next-auth/providers/credentials";
-import clientPromise from "../../../../../lib/mongo/mongodb";
 
 let db;
 let client;
 
-export const options = {
+export default NextAuth({
+  adapter: MongoDBAdapter(clientPromise, {
+    databaseName: "gabeTest",
+  }),
+  callbacks: {
+    signIn: async ({ account, user, credentials, email, profile }) => {
+      console.log("------ SIGN IN ------");
+      console.log({ account, user, credentials, email, profile });
+      return true;
+    },
+    // session: async ({ session, token, user }) => {
+    //   console.log("------ SESSION ------");
+    //   console.log({ session, token, user });
+    //   session.user = token.user;
+    //   // session.status = "authenticated";
+
+    //   // session.admin = true;
+    //   return session;
+    // },
+    // jwt: async ({ token, user }) => {
+    //   console.log("------ JWT ------");
+    //   console.log("token");
+    //   console.log(token);
+    //   console.log("user");
+    //   console.log(user);
+    //   console.log("token.user");
+    //   console.log(token.user);
+    //   user && (token.user = user);
+    //   return token;
+    // },
+  },
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID,
@@ -15,10 +46,10 @@ export const options = {
     CredentialsProvider({
       name: "Credentials",
       credentials: {
-        username: {
-          label: "Username",
+        email: {
+          label: "Email",
           type: "text",
-          placeholder: "nerdslayee0",
+          placeholder: "email@domain.com",
         },
         password: {
           label: "Password",
@@ -28,20 +59,23 @@ export const options = {
       },
       async authorize(credentials) {
         let dbUser;
+        console.log("------ CREDENTIALS ------");
+        console.log(credentials.email);
+        console.log(credentials.password);
         if (!db) {
           try {
             client = await clientPromise;
-            db = await client.db("4921-project1");
+            db = await client.db("gabeTest");
             dbUser = await db
               .collection("users")
-              .findOne({ username: credentials?.username });
+              .findOne({ email: credentials?.email });
             // .select("+password");
           } catch (error) {
             throw new Error("Failed to establish connection to database");
           }
         }
-
         if (credentials?.password === dbUser.password) {
+          console.log(dbUser);
           return dbUser;
         } else {
           return null;
@@ -49,34 +83,89 @@ export const options = {
       },
     }),
   ],
-  //TODO: Pages --> sign up
-  // pages: {
-  //   signUp: "/signUp",
-  // },
-  session: {
-    // jwt: true,
-    strategy: "jwt",
-    // maxAge: 60 * 60,
-    // updateAge: 60 * 60,
-    // generateSessionToken: () => {
-    //   return randomUUID?.() ?? randomBytes(32).toString("hex");
-    // },
-  },
-  callbacks: {
-    async session(session, token) {
-      session.user = token.user;
-      return session;
-    },
-    async jwt(token) {
-      if (typeof user !== typeof undefined) {
-        token.user = username;
-      }
-      return token;
-    },
-  },
-};
+  secret: process.env.NEXTAUTH_SECRET,
+});
 
-export default NextAuth(options);
+// import NextAuth from "next-auth";
+// import GitHubProvider from "next-auth/providers/github";
+// import CredentialsProvider from "next-auth/providers/credentials";
+// import clientPromise from "../../../../../lib/mongo/mongodb";
+
+// let db;
+// let client;
+
+// export const options = {
+//   providers: [
+//     GitHubProvider({
+//       clientId: process.env.GITHUB_ID,
+//       clientSecret: process.env.GITHUB_SECRET,
+//     }),
+//     CredentialsProvider({
+//       name: "Credentials",
+//       credentials: {
+//         username: {
+//           label: "Username",
+//           type: "text",
+//           placeholder: "nerdslayee0",
+//         },
+//         password: {
+//           label: "Password",
+//           type: "text",
+//           placeholder: "none",
+//         },
+//       },
+//       async authorize(credentials) {
+//         let dbUser;
+//         if (!db) {
+//           try {
+//             client = await clientPromise;
+//             db = await client.db("4921-project1");
+//             dbUser = await db
+//               .collection("users")
+//               .findOne({ username: credentials?.username });
+//             // .select("+password");
+//           } catch (error) {
+//             throw new Error("Failed to establish connection to database");
+//           }
+//         }
+
+//         if (credentials?.password === dbUser.password) {
+//           return dbUser;
+//         } else {
+//           return null;
+//         }
+//       },
+//     }),
+//   ],
+//   //TODO: Pages --> sign up
+//   // pages: {
+//   //   signUp: "/signUp",
+//   // },
+//   session: {
+//     // jwt: true,
+//     strategy: "jwt",
+//     // maxAge: 60 * 60,
+//     // updateAge: 60 * 60,
+//     // generateSessionToken: () => {
+//     //   return randomUUID?.() ?? randomBytes(32).toString("hex");
+//     // },
+//   },
+//   callbacks: {
+//     async session(session, token) {
+//       session.user = token.user;
+//       return session;
+//     },
+//     async jwt(token) {
+//       if (typeof user !== typeof undefined) {
+//         token.user = username;
+//       }
+//       return token;
+//     },
+//   },
+//   secret: process.env.NEXTAUTH_SECRET,
+// };
+
+// export default NextAuth(options);
 
 // TODO: https://www.reddit.com/r/nextjs/comments/q02t3y/how_to_send_user_info_in_a_nextauth_session/
 // https://next-auth.js.org/configuration/callbacks#session-callback
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,53 @@
-import { Center, useColorMode } from "@chakra-ui/react";
-import { Button } from "@chakra-ui/react";
-import Link from "next/link";
+import { signIn, signOut, useSession } from "next-auth/react";
 
-function ColorToggler() {
-  const { colorMode, toggleColorMode } = useColorMode();
-  return (
-    <Center>
-      <Button onClick={toggleColorMode}>
-        Toggle {colorMode === "light" ? "Dark" : "Light"}
-      </Button>
-    </Center>
-  );
-}
+const HomePage = () => {
+  const { data, status } = useSession();
 
-export default function Home() {
   return (
     <>
-      <Center margin={4}>
-        <Button>
-          <Link href="/landing">Go to Landing</Link>
-        </Button>
-      </Center>
-      <Center margin={4}>
-        <Button>
-          <Link href="/signUp">Sign Up!</Link>
-        </Button>
-      </Center>
-      <ColorToggler />
+      {JSON.stringify(data || {})}
+      {JSON.stringify(status || {})}
+      {status === "unauthenticated" && (
+        <button onClick={() => signIn()}>Sign In</button>
+      )}
+      {status === "authenticated" && (
+        <button onClick={() => signOut()}>Sign Out</button>
+      )}
     </>
   );
-}
+};
+
+export default HomePage;
+
+// import { Center, useColorMode } from "@chakra-ui/react";
+// import { Button } from "@chakra-ui/react";
+// import Link from "next/link";
+
+// function ColorToggler() {
+//   const { colorMode, toggleColorMode } = useColorMode();
+//   return (
+//     <Center>
+//       <Button onClick={toggleColorMode}>
+//         Toggle {colorMode === "light" ? "Dark" : "Light"}
+//       </Button>
+//     </Center>
+//   );
+// }
+
+// export default function Home() {
+//   return (
+//     <>
+//       <Center margin={4}>
+//         <Button>
+//           <Link href="/landing">Go to Landing</Link>
+//         </Button>
+//       </Center>
+//       <Center margin={4}>
+//         <Button>
+//           <Link href="/signUp">Sign Up!</Link>
+//         </Button>
+//       </Center>
+//       <ColorToggler />
+//     </>
+//   );
+// }
