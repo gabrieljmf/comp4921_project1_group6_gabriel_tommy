df16def64371c0f064003e262177c53b820e733a
Tommy Nguyen <tommylulxdd@gmail.com>
2023-09-14 23:08:23 -0700

    Updated CSS for signup page

--- src/pages/signUp.js
+++ src/pages/signUp.js
@@ -1,4 +1,12 @@
-import { Form, FormLayout, Field, SubmitButton } from "@saas-ui/forms";
+import { Form } from "@saas-ui/forms";
+import {
+  Flex,
+  Center,
+  FormControl,
+  FormLabel,
+  Input,
+  Button,
+} from "@chakra-ui/react";
 import React, { useState } from "react";
 import { useRouter } from "next/router";
 // import mongoose from "mongoose";
@@ -14,10 +22,9 @@ export default function signUp() {
     var pattern = new RegExp(
       "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[-+_!@#$%^&*.,?]).+$"
     );
-
     var minLen = 10;
-
-    if (pattern.test(password) && password.length >= minLen) {
+    console.log(password);
+    if (pattern.test(password)) {
       try {
         const res = await fetch("api/register", {
           method: "POST",
@@ -29,7 +36,6 @@ export default function signUp() {
             password,
           }),
         });
-
         if (res.ok) {
           router.push("/landing");
           console.log("Creating User Successful");
@@ -45,25 +51,53 @@ export default function signUp() {
     }
   };
   return (
-    <Form onSubmit={onSubmit}>
-      <FormLayout>
-        <Field
-          name="username"
-          type="text"
-          label="Username"
-          placeholder="Enter Username"
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <Field
-          name="password"
-          type="password"
-          label="Password"
-          placeholder="Enter Password"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        {/* TODO: Make a CreateCredentials function and pass the formData. async await query, validate, add to db, then redirect */}
-        <SubmitButton>Sign Up</SubmitButton>
-      </FormLayout>
-    </Form>
+    <Flex
+      width={"100vw"}
+      height={"100vh"}
+      alignContent={"center"}
+      justifyContent={"center"}
+      bg="#161b22"
+    >
+      <Center>
+        <Form onSubmit={onSubmit}>
+          <FormControl bg="#0d1117" p="20px 50px" borderRadius="30px">
+            <FormLabel color="white" mb="0rem">
+              Username
+            </FormLabel>
+            <Input
+              type="text"
+              placeholder="Enter Username"
+              mb=".5rem"
+              borderColor="#555"
+              _placeholder={{ color: "#555" }}
+              color="white"
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <FormLabel color="white" mb="0rem">
+              Password
+            </FormLabel>
+            <Input
+              type="password"
+              placeholder="Enter Password"
+              mb=".5rem"
+              borderColor="#555"
+              _placeholder={{ color: "#555" }}
+              color="white"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button
+              m="0 0 .75rem"
+              p=".75rem 1rem"
+              width="100%"
+              bg="#161b22"
+              color="#ccc"
+              type="submit"
+            >
+              Sign Up
+            </Button>
+          </FormControl>
+        </Form>
+      </Center>
+    </Flex>
   );
 }
